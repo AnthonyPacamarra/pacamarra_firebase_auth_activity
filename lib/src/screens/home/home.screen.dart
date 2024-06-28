2440e20322e967d4e91b61f813620cfd1dc61178
AnthonyPacamarra <anthonygwapopacamarra@gmail.com>
2024-06-28 15:00:21 +0800

    firebase-activity

--- lib/src/screens/home/home.screen.dart
+++ lib/src/screens/home/home.screen.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 
+import '../../controllers/auth_controller.dart';
+import '../../dialogs/waiting_dialog.dart';
+import '../../routing/router.dart';
+import '../auth/login.screen.dart';
+
 class HomeScreen extends StatelessWidget {
   static const String route = '/home';
   static const String path = "/home";
@@ -9,6 +14,29 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text("Home"));
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const Center(child: Text("Home")),
+        ElevatedButton.icon(
+          style: ElevatedButton.styleFrom(
+            foregroundColor: Colors.white,
+            backgroundColor: const Color(0xFF242F9B),
+            padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 12),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(12),
+            ),
+            minimumSize: const Size(150, 48),
+          ),
+          onPressed: () {
+            WaitingDialog.show(context, future: AuthController.I.logout());
+            // AuthController.I.logout();
+            // GlobalRouter.I.router.go(LoginScreen.route);
+          },
+          icon: const Icon(Icons.logout_rounded),
+          label: const Text('logout'),
+        )
+      ],
+    );
   }
 }
